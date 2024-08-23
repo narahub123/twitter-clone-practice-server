537d0c791ffb8d7197ed99676610e0eb11fa8add
nara <parknara0213@gmail.com>
2024-08-23 12:14:21 +0900

    240823 몽고db와 연결하기

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
+import connectDB from "./db/connectDB.js";
 
 dotenv.config();
 
+connectDB();
 const app = express();
 
 const PORT = process.env.PORT || 5000;
