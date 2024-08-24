9823062af72294ed1caf7ddd57a927ebda3a52f0
nara <parknara0213@gmail.com>
2024-08-25 03:08:52 +0900

    240826 유저 프로필 정보 얻는 방법 수정

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,13 +2,27 @@ import User from "../models/userModel.js";
 import bcrypt from "bcryptjs";
 import generateTokenAndSetCookie from "../utils/helpers/generateTokenAndSetCookie.js";
 import { v2 as cloudinary } from "cloudinary";
+import mongoose from "mongoose";
+
 const getUserProfile = async (req, res) => {
-  const { username } = req.params;
+  // we will fetch user profile either username or userId
+  // query is either username or userId
+  const { query } = req.params;
 
   try {
-    const user = await User.findOne({ username })
-      .select("-password")
-      .select("-updatedAt");
+    let user;
+
+    // query is userId
+    if (mongoose.Types.ObjectId.isValid(query)) {
+      user = await User.findOne({ _id: query })
+        .select("-password")
+        .select("-updatedAt");
+    } else {
+      user = await User.findOne({ username: query })
+        .select("-password")
+        .select("-updatedAt");
+    }
+
     if (!user) return res.status(400).json({ error: "User not found" });
 
     return res.status(200).json(user);
